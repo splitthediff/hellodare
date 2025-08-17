e5f1712c0990063b616ac18f09a8379cc0821025
Leanne Dare <leanne@hellodare.com>
2025-08-16 20:55:19 -0700

    build: configure for multi-page application UGH

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,25 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
+import { glob } from 'glob'
+
+const htmlFiles = glob.sync(['src/**/*.html', '!src/vite-env.d.ts'])
+
+const input = htmlFiles.reduce((acc, file) => {
+  const name = file.replace('src/', '').replace('.html', '')
+  acc[name] = resolve(__dirname, file)
+  return acc
+}, {})
 
 export default defineConfig({
-
   root: 'src',
-
   base: '/hellodare/',
+  publicDir: '../public',
 
   build: {
     outDir: '../dist',
-    emptyOutDir: true
-  }
+    rollupOptions: {
+      input: input,
+    },
+    emptyOutDir: true,
+  },
 })
